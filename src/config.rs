80d95b7302bb53b3c2c484fa8fba2f006a54161b
Danilo <ddjokic@boylesports.com>
2023-08-16 14:45:44 +0200

    Change config from json to toml

--- src/config.rs
+++ src/config.rs
@@ -9,19 +9,9 @@ pub struct Config {
     pub initial_field_size: usize
 }
 
-// fn default_config() -> Config {
-//     Config {
-//         width: 100,
-//         height: 30,
-//         spawn_rate: 1.0,
-//         initial_field_size: 10
-//      } 
-// }
-
-
 impl Config {
     pub fn init_from_file() -> Self {
-        let config = Config::from_config_file("./config.json").unwrap();
+        let config = Config::from_config_file("./config.toml").unwrap();
         config
     }
 }
